d544f7090074021fc74109818ec71041b6e2e459
amitkahlon <amitkahlon4321@gmail.com>
2020-11-22 13:39:31 +0200

    upload

--- WpfNetCoreMvvm/WpfNetCoreMvvm/ViewModels/HomeViewModel.cs
+++ WpfNetCoreMvvm/WpfNetCoreMvvm/ViewModels/HomeViewModel.cs
@@ -1,12 +1,31 @@
 ﻿using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ChatClient.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
-        
+        public ObservableCollection<User> Users { get; set; }
+
+        public HomeViewModel()
+        {
+            Users = new ObservableCollection<User>()
+            {
+                new User
+                {
+                    name = "amit",
+                    age = 21
+                }
+            };
+        }
+    }
+
+    public class User
+    {
+        public int age { get; set; }
+        public string name{ get; set; }
     }
 }
